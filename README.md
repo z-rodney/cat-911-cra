28b9501939d68516b4cae21f51e536b33cbccf97
Zaina Rodney <zainarodney@gmail.com>
2021-05-03 18:05:13 -0400

    added readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# Welcome to CAT911's Community Resources App
+
+This app provides abolitionist/decarceral community resources located in California.
+
+To run this app locally:
+1. Install the libraries used in this project:
+`npm install`
+2. Build the project using parcel:
+`npm run build:dev`
+3. Open in your browser:
+`localhost:1234`
--- components/ResourceDetail.js
+++ components/ResourceDetail.js
@@ -15,7 +15,7 @@ const ResourceDetail = () => {
             <h2>{organization}</h2>
             <p>Location: {address}</p>
             <p>Phone Number: {phoneNumber}</p>
-            <p>Website: {website ? <a href={website}>{website}</a> :'None'}</p>
+            <p>Website: {website ? <a href={website} target="_blank" rel="noopener noreferrer">{website}</a> :'None'}</p>
           </div>
         ) :
         (
